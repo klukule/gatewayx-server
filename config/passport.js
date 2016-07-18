19368f73383d0dfa4e4f09aebc1c6d22900b644a
Lukáš Jech <klukule@moowdesign.eu>
2016-07-18 22:52:14 +0200

    Fixed crashing /memberinfo
    fixed crashing when using endpoint /memberinfo with bad or missing header

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,12 @@
-var JwtStrategy = require('passport-jwt').Strategy;
-
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+    
 var User = require('../app/models/user');
 var config = require('../config/database');
 
 module.exports = function(passport) {
   var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({id: jwt_payload.id}, function(err, user) {
